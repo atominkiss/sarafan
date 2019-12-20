aad0fe379aa7bde0e2a1f1354bc519127732f344
atomink <atomink@gmail.com>
2019-12-20 23:20:26 +0300

    Initial commit

---
+++ src/test/java/net/protoprint/sarafan/SarafanApplicationTests.java
@@ -0,0 +1,13 @@
+package net.protoprint.sarafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
